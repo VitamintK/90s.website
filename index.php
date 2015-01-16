b5f689b947b6330da355cd068c3b87a3ea58e973
KEvin Wang <kevinwang@kevinwang.us>
2015-01-16 13:14:31 -0800

    PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title> frosted tips </title>
+		<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+	</head>
+	<body background = "deep.gif">
+		90s website!!!!!!!!!!!!!!
+		<img src="aw20.gif" alt="UNDER CONSTRUCTION">
+	</br>
+	php is the best dev language!!!!!!!!
+	</br>
+	about me:
+	<p>
+		I am a php programmer I am very professional just like my friend Saikaran
+
+	</p>
+		<img src = "frostedtips.png" alt = "FROSTEDTIPS">
+	</body>
+</html>
